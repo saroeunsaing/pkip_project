ae0e1de7f029c6f453b08b1b8e180e87c29d9009
kheasam <kheasam058@gmail.com>
2022-05-30 08:40:32 +0700

    hello

---
+++ pkip_project/Forms/Frm_Academic.Designer.vb
@@ -0,0 +1,41 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_Academic
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'Frm_Academic
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 27.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightCoral
+        Me.ClientSize = New System.Drawing.Size(1100, 665)
+        Me.Font = New System.Drawing.Font("Khmer OS Siemreap", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.Name = "Frm_Academic"
+        Me.Text = "Frm_Academic"
+        Me.ResumeLayout(False)
+
+    End Sub
+End Class
